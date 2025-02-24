dc44fd51cd4dadc90a5630fa9ed188f2cd6eb1b3
ghibrahim <ghibrahimm@gmail.com>
2025-02-24 18:40:43 +0100

    adding dynamic tempaltes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 # This is the simplest view possible in Django.
 # To create a view, we created a function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html.
