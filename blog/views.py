d15a19a5c77a2c8ce7c2c0a024db13ac4ac6f33e
ghibrahim <ghibrahimm@gmail.com>
2025-02-24 10:07:35 +0100

    updated structure and views - still not dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+# This is the simplest view possible in Django.
+# To create a view, we created a function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html.
